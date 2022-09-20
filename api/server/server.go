149ecfb48b0c5d018feb15548095ff7fe4f4f1d7
Luke Teo <yipengteo.luke@gmail.com>
2022-09-20 11:33:42 +0800

    setup server complete

---
+++ api/server/server.go
@@ -0,0 +1,27 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/trademate-be/internal/controller/sqlc/store"
+)
+
+type Server struct {
+	store  store.Store
+	router *gin.Engine
+}
+
+func NewServer(store store.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
